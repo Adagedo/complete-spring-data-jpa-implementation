831acaf2516aa6fa8536b410047e66b43aef5ca1
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-06-09 12:53:50 +0100

    created the author repository

---
+++ src/main/java/com/adagedo_softengineer/Jpa/models/AuthorRepository.java
@@ -0,0 +1,6 @@
+package com.adagedo_softengineer.Jpa.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Integer>{
+}
