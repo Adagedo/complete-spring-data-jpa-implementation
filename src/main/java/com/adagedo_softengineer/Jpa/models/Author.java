8e01ff70c94d86fad11e7876402bbc53279f9b0f
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-06-09 11:40:25 +0100

    added boiler plates annotations

--- src/main/java/com/adagedo_softengineer/Jpa/models/Author.java
+++ src/main/java/com/adagedo_softengineer/Jpa/models/Author.java
@@ -1,10 +1,25 @@
 package com.adagedo_softengineer.Jpa.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter // implements getters for the author fields 
+@Setter // implements setters
+@NoArgsConstructor // creates a No argument constructor
+@AllArgsConstructor // creates constructor with arguments
+
 public class Author {
 
     private Integer id;
+
     private String firstname;
+
     private String lastname;
+
     private String email;
+
     private Integer age;
+
 }
