ccd02c93ab1401955f35b0c7ce2facd29a514ca9
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-06-09 14:21:50 +0100

    created relationship btwn the section aand lecture entity

--- src/main/java/com/adagedo_softengineer/Jpa/models/Lecture.java
+++ src/main/java/com/adagedo_softengineer/Jpa/models/Lecture.java
@@ -3,6 +3,8 @@ package com.adagedo_softengineer.Jpa.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,4 +23,7 @@ public class Lecture {
 
     private String name;
 
+    @ManyToOne
+    @JoinColumn(name="section_id")
+    private Section section;
 }
--- src/main/java/com/adagedo_softengineer/Jpa/models/Section.java
+++ src/main/java/com/adagedo_softengineer/Jpa/models/Section.java
@@ -1,10 +1,13 @@
 package com.adagedo_softengineer.Jpa.models;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,5 +31,10 @@ public class Section {
     @ManyToOne
     @JoinColumn(name="course_id")
     private Course course;
+
+    @OneToMany(
+        mappedBy = "section"
+    )
+    private List<Lecture> lectures;
 
 }
