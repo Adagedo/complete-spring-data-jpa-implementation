6f39e0d22ac6acd1ca017c74679c9c7b25db626c
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-06-09 12:42:54 +0100

    completed the author table

--- src/main/java/com/adagedo_softengineer/Jpa/models/Author.java
+++ src/main/java/com/adagedo_softengineer/Jpa/models/Author.java
@@ -1,5 +1,11 @@
 package com.adagedo_softengineer.Jpa.models;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,16 +15,29 @@ import lombok.Setter;
 @Setter // implements setters
 @NoArgsConstructor // creates a No argument constructor
 @AllArgsConstructor // creates constructor with arguments
+@Entity
 public class Author {
 
+    @Id
+    @GeneratedValue // auto generate our id
+    @Column(unique = true)
     private Integer id;
 
+    @Column(nullable = false, length=20)
     private String firstname;
 
+    @Column(nullable = false, length = 20)
     private String lastname;
 
+    @Column(unique=true, nullable=false)
     private String email;
 
     private Integer age;
+
+    @Column(updatable=false, nullable=false)
+    private LocalDateTime createdAt;
+
+    @Column(insertable = false)
+    private LocalDateTime lastModified;
 
 }
