77d7c440fa4d8000910a05a20bbbec3f459ab38b
Adagedo Solomon Israel <adagedosolomon52@gmail.com>
2025-06-09 11:27:23 +0100

    created the models folder with an author java class

---
+++ src/main/java/com/adagedo_softengineer/Jpa/models/Author.java
@@ -0,0 +1,10 @@
+package com.adagedo_softengineer.Jpa.models;
+
+public class Author {
+
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private Integer age;
+}
